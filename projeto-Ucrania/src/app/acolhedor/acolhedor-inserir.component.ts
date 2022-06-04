a82ea0bfeb033ea895c901eaa1ffa6baa612ff5c
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-06-04 10:03:34 -0300

    Continuando Acolhedor

--- projeto-Ucrania/src/app/acolhedor/acolhedor-inserir.component.ts
+++ projeto-Ucrania/src/app/acolhedor/acolhedor-inserir.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 @Component({
 selector: 'app-acolhedor-inserir',
 templateUrl: './acolhedor-inserir.component.html',
+styleUrls: ['./acolhedor-inserir.component.css'],
 })
 export class AcolhedorInserirComponent {
-
+  nome: string;
+  fone: string;
+  email: string;
   onAdicionarAcolhedor() {
     console.log('inserindo acolhedor...');
     }
--- projeto-Ucrania/src/app/app.module.ts
+++ projeto-Ucrania/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AcolhedorInserirComponent } from './acolhedor/acolhedor-inserir.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { AcolhedorListaComponent } from './acolhedor/acolhedor-lista/acolhedor-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent, AcolhedorInserirComponent
+    AppComponent, AcolhedorInserirComponent, CabecalhoComponent, AcolhedorListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
