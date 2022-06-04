e26e8cc9ab8a58e3509a9f6a24eb48043a0c17c2
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-06-04 10:03:54 -0300

    Criando acolhedor-lista

---
+++ projeto-Ucrania/src/app/acolhedor/acolhedor-lista/acolhedor-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acolhedor-lista',
+  templateUrl: './acolhedor-lista.component.html',
+  styleUrls: ['./acolhedor-lista.component.css']
+})
+export class AcolhedorListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
