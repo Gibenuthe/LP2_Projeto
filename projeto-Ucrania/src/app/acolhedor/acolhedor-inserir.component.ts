1f6cc6df531aa265e74cbeca92dd588df44198a5
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-06-04 09:24:34 -0300

    Inicio Acolhedor

---
+++ projeto-Ucrania/src/app/acolhedor/acolhedor-inserir.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+selector: 'app-acolhedor-inserir',
+templateUrl: './acolhedor-inserir.component.html',
+})
+export class AcolhedorInserirComponent {
+
+  onAdicionarAcolhedor() {
+    console.log('inserindo acolhedor...');
+    }
+
+}
+
